b52a6d757f32ea64679dd085b065ab06407abafd
Monika Lenartowicz <nika.nel@interia.pl>
2024-01-23 15:37:42 +0100

    Added display products functionality  and filter by search input

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('filters project');
+let filteredProducts = [...products];
+
+const productsContainer = document.querySelector(".products-container");
+
+const displayProducts = () => {
+	// if statement
+	if (filteredProducts.length < 1) {
+		productsContainer.innerHTML = `<h6>Sorry, no products matched your search</h6>`;
+		return;
+	}
+	productsContainer.innerHTML = filteredProducts
+		.map(product => {
+			const { id, image, title, price } = product;
+			return `
+      <article class="product" data-id="${id}">
+          <img src="${image}" alt="" class="img product-img">
+          <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">$${price}</span>
+          </footer>
+      </article>
+    `;
+		})
+		.join("");
+};
+
+displayProducts();
+
+// filters
+const form = document.querySelector(".input-form");
+const searchInput = document.querySelector(".search-input");
+
+form.addEventListener("keyup", () => {
+	const inputValue = searchInput.value;
+
+	filteredProducts = products.filter(product => {
+		// console.log(product.title.includes(inputValue));
+		return product.title.toLowerCase().includes(inputValue);
+	});
+
+	displayProducts();
+});
